9d397794cc97201956c199cfc3e713508153acb1
julianperrone <julianperrone555@live.com>
2023-04-14 15:45:56 -0300

    Hamburger menu and some query updates
    Hamburger menu and some updates on the media querys and styles css. More media querys configurations are required on mobile mode

--- scripts.js
+++ scripts.js
@@ -20,3 +20,28 @@ async function handleSubmit(event) {
     alert('¡Gracias por contactarte! Tu consulta sera respondida a la brevedad.')
   }
 }
+
+/* Hamburguer Menu by Bing ChatGPT */
+
+var button = document.querySelector('#hamburger-button');
+var menu = document.querySelector('.left-section');
+
+// Agrega un controlador de eventos para el botón
+button.addEventListener('click', function() {
+    // Muestra u oculta el menú al hacer clic en el botón
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
+// Selecciona todos los enlaces del menú
+var links = menu.querySelectorAll('a');
+
+// Agrega un controlador de eventos para cada enlace
+links.forEach(function(link) {
+    link.addEventListener('click', function() {
+        // Oculta el menú al hacer clic en un enlace
+        menu.style.display = 'none';
+    });
+});
